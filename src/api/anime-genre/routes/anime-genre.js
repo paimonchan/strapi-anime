712621fe7cc3fe339456abb173e2c8af629e3137
Paimonchan <n.h.shounen@gmail.com>
2023-07-24 19:13:24 +0700

    [NA] add new collection for anime-genre

---
+++ src/api/anime-genre/routes/anime-genre.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * anime-genre router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::anime-genre.anime-genre');
---
+++ src/api/anime-genre/services/anime-genre.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * anime-genre service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::anime-genre.anime-genre');
