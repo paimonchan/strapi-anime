179cc10ee9c43262911ef2c625f120d604712ec3
Paimonchan <n.h.shounen@gmail.com>
2023-07-31 00:06:34 +0700

    [NA] auto create cronjob record based on configuration

--- src/index.js
+++ src/index.js
@@ -10,12 +10,28 @@ module.exports = {
    */
   register(/*{ strapi }*/) {},
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/*{ strapi }*/) {},
+    /**
+     * An asynchronous bootstrap function that runs before
+     * your application gets started.
+     *
+     * This gives you an opportunity to set up your data model,
+     * run jobs, or perform some special logic.
+     */
+    async bootstrap({strapi}) {
+        console.log(strapi.cron)
+        const cronjobService = strapi.service('api::cronjob.cronjob')
+        const cronsConfig = require('../cronjob')
+        for (const name in cronsConfig) {
+            const cronConfig = cronsConfig[name]
+            cronConfig.cron.name = name
+
+            const {active} = cronConfig
+            const cron = await cronjobService.getCron(name)
+            if (!cron) {
+                await cronjobService.createCron(name, active)
+            } else {
+                await cronjobService.writeCron(cron, name, active)
+            }
+        }
+    },
 };
