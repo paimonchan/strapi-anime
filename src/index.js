5f88b8079e862acbe5c5f776f335ad9ffd66de16
Paimonchan <n.h.shounen@gmail.com>
2023-08-01 17:52:29 +0700

    [NA] refactor, adjust indentation line

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@
 
 /** strapi life cycle: https://docs.strapi.io/dev-docs/configurations/functions */
 module.exports = {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/*{ strapi }*/) {},
+    /**
+     * An asynchronous register function that runs before
+     * your application is initialized.
+     *
+     * This gives you an opportunity to extend code.
+     */
+    register(/*{ strapi }*/) {},
 
     /**
      * An asynchronous bootstrap function that runs before
