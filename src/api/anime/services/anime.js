9d0e7a3694335b4fe47442e582b4fc2242255820
Paimonchan <n.h.shounen@gmail.com>
2023-08-08 22:05:01 +0700

    [NA] add anime model functioin

--- src/api/anime/services/anime.js
+++ src/api/anime/services/anime.js
@@ -3,7 +3,62 @@
 /**
  * anime service
  */
-
+const MODEL = 'api::anime.anime'
+const MODEL_ANIME_MAP = 'api::anime-map.anime-map'
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::anime.anime');
+module.exports = createCoreService('api::anime.anime', ({strapi}) => ({
+    async getAnimeBySourceId (transacting, sourceId, source) {
+        const records = await strapi.entityService.findMany(MODEL, {
+            transacting,
+            limit               : 1,
+            filters             : {
+                anime_maps      : {
+                    source      : source,
+                    txt_map_id  : sourceId,
+                }
+            }
+        })
+        return records.length > 0 ? records[0] : false
+    },
+
+    async getAnimeBySourceOrderByLastCheck (transacting, source, limit=10) {
+        /** @ref https://github.com/knex/knex/pull/4720/files */
+        /** add this PR to order record by null first/last */
+
+        /** TODO: filter by exclude completed anime */
+        const params = (isnull) => {
+            return {
+                transacting,
+                limit               : limit,
+                filters             : {
+                    anime_maps      : {
+                        source      : source,
+                        last_check  : {$null: isnull}
+                    }
+                },
+                orderBy             : {
+                    anime_maps      : {
+                        last_check  : 'ASC',
+                        name        : 'ASC'
+                    }
+                }
+            }
+        }
+
+        /** check for null first last_check field, if exist return it */
+        let records = await strapi.entityService.findMany(MODEL, params(true))
+        if (records.length > 0) {
+            return records
+        }
+
+        /** return for not null last_check */
+        records = await strapi.entityService.findMany(MODEL, params(false))
+        return records
+    },
+
+    async getAll (transacting) {
+        const records = await strapi.entityService.findMany(MODEL, {transacting})
+        return records
+    },
+}));
