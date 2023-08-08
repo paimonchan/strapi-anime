f5ba7c4ecfc40fe3682952b52fc775d72e206189
Paimonchan <n.h.shounen@gmail.com>
2023-08-08 22:06:23 +0700

    [NA] add anime genre model function

--- src/api/anime-genre/services/anime-genre.js
+++ src/api/anime-genre/services/anime-genre.js
@@ -3,7 +3,12 @@
 /**
  * anime-genre service
  */
-
+const MODEL = 'api::anime-genre.anime-genre'
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::anime-genre.anime-genre');
+module.exports = createCoreService('api::anime-genre.anime-genre', ({strapi}) => ({
+    async getAll (transacting) {
+        const records = await strapi.entityService.findMany(MODEL, {transacting})
+        return records
+    }
+}));
