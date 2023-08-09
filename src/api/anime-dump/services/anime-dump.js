f7be9b70839f290ed1830a6c5d1f7d24cca4b62a
Paimonchan <n.h.shounen@gmail.com>
2023-08-09 22:50:19 +0700

    [NA] add model function for non deleted record

--- src/api/anime-dump/services/anime-dump.js
+++ src/api/anime-dump/services/anime-dump.js
@@ -3,7 +3,22 @@
 /**
  * anime-dump service
  */
-
+const MODEL = 'api::anime-dump.anime-dump'
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::anime-dump.anime-dump');
+module.exports = createCoreService('api::anime-dump.anime-dump', ({strapi}) => ({
+    async getNonDelete(transacting, limit = 100) {
+        const records = await strapi.entityService.findMany(MODEL, {
+            transacting,
+            limit           : limit,
+            sort            : {id: 'asc'},
+            filters         : {
+                $or             : [
+                    {to_delete  : {$null: true}},
+                    {to_delete  : false},
+                ]
+            }
+        })
+        return records
+    },
+}));
