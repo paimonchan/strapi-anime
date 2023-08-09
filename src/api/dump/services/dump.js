2136dd1205b1726a2812b097375e5cd639166e94
Paimonchan <n.h.shounen@gmail.com>
2023-08-10 00:02:38 +0700

    [NA] add function model to check existing record

--- src/api/dump/services/dump.js
+++ src/api/dump/services/dump.js
@@ -6,4 +6,17 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::dump.dump');
+module.exports = createCoreService('api::dump.dump', ({strapi}) => ({
+    async checkExisting(transacting, resId, resDate) {
+        const existing = await strapi.entityService.findMany('api::dump.dump', {
+            transacting,
+            limit           : 1,
+            filters         : {
+                res_id      : resId,
+                res_date    : resDate,
+            }
+        })
+        const exist = existing.length  > 0
+        return exist
+    }
+}));
