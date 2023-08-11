f3b27b414293c510f1bbc46b2be38fb3180af0ac
Paimonchan <n.h.shounen@gmail.com>
2023-08-11 18:09:51 +0700

    [NA] add anime module to get anime by source

--- src/api/anime-map/services/anime-map.js
+++ src/api/anime-map/services/anime-map.js
@@ -4,6 +4,21 @@
  * anime-map service
  */
 
+const MODEL = 'api::anime-map.anime-map'
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::anime-map.anime-map');
+module.exports = createCoreService('api::anime-map.anime-map', ({strapi}) => ({
+    async getAnimeMapBySource (source, animeId) {
+        const records = await strapi.entityService.findMany(MODEL, {
+            limit           : 1,
+            filters         : {
+                source      : source,
+                anime       : {
+                    id      : animeId
+                }
+            }
+        })
+
+        return records.length > 0 ? records[0] : false
+    }
+}));
