918852a68abe41ff78feeccf5b1134f87de53bf5
Paimonchan <n.h.shounen@gmail.com>
2023-08-15 21:14:28 +0700

    [NA] enable cronjob

--- config/server.js
+++ config/server.js
@@ -1,3 +1,6 @@
+// const crontTasks = require("./cron-tasks");
+const cronTasks = require('../cronjob')
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
@@ -6,5 +9,9 @@ module.exports = ({ env }) => ({
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  cron: {
+    enabled: env.bool("CRON_ENABLED", true),
+    tasks: cronTasks,
   },
 });
